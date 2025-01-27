9ae1638e0a4b1bf32ed401e1947bca5bb3dc9df9
mahlet <mahlet2ambaye@gmail.com>
2025-01-27 17:12:25 +0300

    payment_integration

--- lib/payment/page.dart
+++ lib/payment/page.dart
@@ -1,52 +1,110 @@
-// import 'package:flutter/material.dart';
-// // import 'package:chapasdk/chapasdk.dart';
-//  import 'package:stripe_payment/stripe_payment.dart';
-
-// class PaymentScreen extends StatefulWidget {
-//   @override
-//   _PaymentScreenState createState() => _PaymentScreenState();
-// }
-
-// class _PaymentScreenState extends State<PaymentScreen> {
-//   // Replace with your actual public key from Chapa
-//   // final String chapaPublicKey = 'CHAPUBK_TEST-xCZA8lT6qIDmBw9b8qjyfdxF9UpBeaB2';
-
-//     Future<void> processPayment() async {
-//      // Create a card
-//      var card = CreditCard(
-//        number: '4242424242424242',
-//        expMonth: 12,
-//        expYear: 23,
-//        cvc: '123',
-//      );
-
-//      // Create a payment method
-//      try {
-//        final paymentMethod = await StripePayment.createPaymentMethod(
-//          PaymentMethodRequest(card: card),
-//        );
-
-//        // Here you can send paymentMethod.id to your server for processing
-//        print("Payment Method ID: ${paymentMethod.id}");
-//      } catch (error) {
-//        print("Error creating payment method: $error");
-//      }
-//    }
-   
-
-   
+import 'package:chapa_unofficial/chapa_unofficial.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text('Chapa Payment')),
-//       body: Center(
-//         child: ElevatedButton(
-//           onPressed: processPayment,
-//           child: Text('Pay with Chapa'),
-//         ),
-//       ),
-//     );
-//   }
-// }
+void main() async {
+  runApp(const MyApp());
+
+  // setup chapa
+  Chapa.configure(privateKey: 'CHAPUBK_TEST-admzj6gPCjctYCqKWZNhwuobKN2YopmP');
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+} 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // final int _counter = 0;
+
+  Future<void> verify() async {
+    Map<String, dynamic> verificationResult =
+        await Chapa.getInstance.verifyPayment(
+      txRef: TxRefRandomGenerator.gettxRef,
+    );
+    if (kDebugMode) {
+      print(verificationResult);
+    }
+  }
+
+  Future<void> pay() async {
+    try {
+      // Generate a random transaction reference with a custom prefix
+      String txRef = TxRefRandomGenerator.generate(prefix: 'linat');
+
+      // Access the generated transaction reference
+      String storedTxRef = TxRefRandomGenerator.gettxRef;
+
+      // Print the generated transaction reference and the stored transaction reference
+      if (kDebugMode) {
+        print('Generated TxRef: $txRef');
+        print('Stored TxRef: $storedTxRef');
+      }
+      await Chapa.getInstance.startPayment(
+        context: context,
+        onInAppPaymentSuccess: (successMsg) {
+          // Handle success events
+        },
+        onInAppPaymentError: (errorMsg) {
+          // Handle error
+        },
+        amount: '1000',
+        currency: 'ETB',
+        txRef: storedTxRef,
+      );
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Linat's payment"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'pay to linat416',
+            ),
+            TextButton(
+                onPressed: () async {
+                  await pay();
+                },
+                child: const Text("Pay")),
+            TextButton(
+                onPressed: () async {
+                  await verify();
+                },
+                child: const Text("Verify")),
+          ],
+        ),
+      ),
+    );
+  }
+}
